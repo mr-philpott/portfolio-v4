6d6a979f6678baedd2ce9770eaa7389166257967
Ethan Philpott <ephilpott2004@gmail.com>
2021-05-07 02:19:06 -0700

    Worked on the title component and set up a few basic classes in the scss

---
+++ src/components/Title.jsx
@@ -0,0 +1,23 @@
+const Title = () => {
+    return (
+        <>
+            <header>
+                <div className="title">
+                    <h6>&lt; h1 &gt;</h6>
+                    <h1>
+                        <div className="selc-blue"><span className="txt-blue selc-blue">E</span>than</div> 
+                        <div className="push-right selc-red"><span className="txt-red selc-red">P</span>hilpott</div>
+                    </h1>
+                    <h6>&lt; /h1 &gt;</h6>
+                </div>
+                <div className="subheading">
+                    <h6>&lt; h2 &gt;</h6>
+                    <h2>A Young <span className="selc-blue und-blue">Programmer</span> Infatuated With <span className="selc-red und-red">Code</span></h2>
+                    <h6>&lt; /h2 &gt;</h6>
+                </div>
+            </header>  
+        </>
+    )
+}
+
+export default Title
